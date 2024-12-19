06b133df776513933ce990f2c5e01d2014d2146c
ayman <aymanehab2002@gmail.com>
2024-12-19 04:20:18 +0200

    for-foreach

--- Demo4/Program.cs
+++ Demo4/Program.cs
@@ -19,11 +19,31 @@
 
             //for (int i = 1; i <= 10; i++)
             //{
-                //Console.WriteLine($"{i} T");
+            //Console.WriteLine($"{i} T");
             //}
 
 
             #endregion
+            #region for-foreach
+            //int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //for (int i = 0; i < Numbers.Length; i++)
+            //{
+            //   // Numbers[i] += 10;
+            //   if (Numbers[i] == 5)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(Numbers[i] );
+            //}
+
+            //class implement interface iemurabel
+            //foreach (int number in Numbers)
+            //{
+            //    number += 10;//invalid
+            //    Console.WriteLine(number);
+            //}
+            #endregion
+
             #endregion
         }
     }

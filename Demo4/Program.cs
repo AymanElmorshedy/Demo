2cb1c02ae2d9f1f61d96ad2e589bec9abe47291a
BassimEhab <bassimstudy@gmail.com>
2024-12-21 00:31:32 +0200

    Q11

--- Demo4/Program.cs
+++ Demo4/Program.cs
@@ -354,14 +354,32 @@ namespace Demo4
             //}
             #endregion
             #region Q10
-            Console.WriteLine("please enter first string");
-            string st1 = Console.ReadLine();
-            Console.WriteLine("please enter  second string ");
-            string st2= Console.ReadLine();
-            string FullSt = st1 + st2;
-            Console.WriteLine(FullSt);
+            //Console.WriteLine("please enter first string");
+            //string st1 = Console.ReadLine();
+            //Console.WriteLine("please enter  second string ");
+            //string st2= Console.ReadLine();
+            //string FullSt = st1 + st2;
+            //Console.WriteLine(FullSt);
+            #endregion
+            #region Q11
+            Console.WriteLine("plese enter degree of marks");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("1");
+                    }
+                    else
+                    {
+                        Console.Write("0");
+                    }
 
-            
+                }
+                Console.WriteLine();
+            }
             #endregion
 
             #endregion

ab01ec23ddaf2cca4f5832f371657159eec59877
BassimEhab <bassimstudy@gmail.com>
2024-12-21 00:19:19 +0200

    Q9

--- Demo4/Program.cs
+++ Demo4/Program.cs
@@ -319,23 +319,38 @@ namespace Demo4
             //Console.WriteLine(BinaryNumber);
             #endregion
             #region Q8
-            Console.WriteLine("plese enter degree of marks");
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 0; i < n; i++)
+            //Console.WriteLine("plese enter degree of marks");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        if (i == j)
+            //        {
+            //            Console.Write("1");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("0");
+            //        }
+
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+            #region Q9
+            //9- Write C# program that Extract a substring from a given string.
+            Console.WriteLine("please enter a string ");
+            string str = Console.ReadLine();
+            int start, end;
+            Console.WriteLine("please enter start");
+            start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter end");
+            end = int.Parse(Console.ReadLine());
+            for (int i = start; i <= end; i++)
             {
-                for (int j = 0; j < n; j++)
-                {
-                    if (i == j)
-                    {
-                        Console.Write("1");
-                    }
-                    else
-                    {
-                        Console.Write("0");
-                    }
-                   
-                }
-                Console.WriteLine();
+                Console.Write(str[i]);
             }
             #endregion
             #endregion

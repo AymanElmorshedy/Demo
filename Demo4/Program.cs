fc687012d90a1a777607e68368fb81cefd2892b1
BassimEhab <bassimstudy@gmail.com>
2024-12-21 02:38:21 +0200

    Q18

--- Demo4/Program.cs
+++ Demo4/Program.cs
@@ -430,7 +430,7 @@ namespace Demo4
             #endregion
             #region Q16
             //int[] Numbers = new int[4] { 1, 2, 3, 4 };
-            //int Max = 0, Min = int.MaxValue,MinInd=0,MaxInd=0;
+            //int Max = 0, Min = int.MaxValue, MinInd = 0, MaxInd = 0;
             //for (int i = 0; i < Numbers.Length; i++)
             //{
             //    if (Numbers[i] > Max)
@@ -444,19 +444,49 @@ namespace Demo4
             //        MinInd = i;
             //    }
             //}
-            //Numbers[MaxInd] = Numbers[MinInd] ;
-            //Max = Numbers[MaxInd] ;
+            //Numbers[MaxInd] = Numbers[MinInd];
+            //Max = Numbers[MaxInd];
             //for (int i = 0; i < Numbers.Length; i++)
             //{
-            //    if(Numbers[i] > Max)
-            //         Max = Numbers[i];
+            //    if (Numbers[i] > Max)
+            //        Max = Numbers[i];
             //}
             //Console.WriteLine($"Max element is {Max}");
             #endregion
+            #region Q17
+
+            #endregion
+            #region Q18
+            Console.WriteLine("please enter number of rows");
+            int rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter a number of coulmns");
+            int columns = int.Parse(Console.ReadLine());
+            int[,] Numbers1 = new int[rows, columns];
+            int[,] Numbers2 = new int[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                Console.WriteLine($"enter value of row{i+1}");
+                
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine("please enter value of coulmns ");
+                    Numbers1[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Array.Copy(Numbers1,Numbers2 ,Numbers2.Length);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.WriteLine(Numbers2[i,j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            #endregion
             #region Q19
-            int[] Numbers = { 1, 2, 4, 5, 6, 7 };
-            for (int i = 0; i < Numbers.Length; i++)
-                Console.Write($"{Numbers[Numbers.Length - 1 - i]} ,");
+            //int[] Numbers = { 1, 2, 4, 5, 6, 7 };
+            //for (int i = 0; i < Numbers.Length; i++)
+            //    Console.Write($"{Numbers[Numbers.Length - 1 - i]} ,");
 
             #endregion
 
